fb90619b70d9fbbc931421cbc9806555b92b8186
Fernanda Nunes <fernanda.nunes@ibm.com>
2020-09-10 00:17:08 -0300

    test in json

--- connectors/apiGithub.js
+++ connectors/apiGithub.js
@@ -1,50 +1,79 @@
 var axios = require("axios");
+const Lime = require('lime-js');
+
 
 function returnDataGithub() {
   url = "https://api.github.com/orgs/takenet/repos?q=language:C#&order=cresc";
   return axios.get(url).then((response) => {
     let filterTweets = response.data.map((result) => {
       let newObject = {
-        itemType: "application/vnd.lime.document-select+json",
-        items: [
-          {
-            header: {
-              type: "application/vnd.lime.media-link+json",
-              "value": {
-                title: "Title",
-                text: "Thisisafirstitem",
-                type: "image/jpeg",
-                uri: result.image
-        
-      }
-      },
-      options: [
-        {
-          label: {
-            type: "application/vnd.lime.web-link+json",
-            value: {
-              title: "Link",
-              uri: "http://www.adoteumgatinho.org.br"
-            }
-          }
-        },
-        {
-          label: {
-            type: "text/plain",
-            "value": "Text 1"
-          },
-          value: {
-            type: "application/json",
-            value: {
-              key1: "value1",
-              key2: "2"
-            }
-          }
-        }
+        type: "application/vnd.lime.collection+json",
+                to: 'teste', 
+                id: Lime.Guid(),
+                content: {
+                    itemType: "application/vnd.lime.document-select+json",
+                    items: [
+                        {
+                            header: {
+                                type: "application/vnd.lime.media-link+json",
+                                value: {
+                                    title: "teste",
+                                    text: "teste",
+                                    type: "image/jpeg",
+                                    uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
+                                }
+                            
+                            }
+                        },
+                        {
+                            header: {
+                                type: "application/vnd.lime.media-link+json",
+                                value: {
+                                    title: "teste",
+                                    text: "teste",
+                                    type: "image/jpeg",
+                                    uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
+                                }
+                            }
+                        },
+                        {
 
-      
-      ]
-      }]}
+                            header: {
+                                type: "application/vnd.lime.media-link+json",
+                                value: {
+                                    title: "teste",
+                                    text: "tal",
+                                    type: "image/jpeg",
+                                    uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
+                                }
+                            }
+                        },
+                        {
+                            header: {
+                                type: "application/vnd.lime.media-link+json",
+                                value: {
+                                    title: "teste",
+                                    text: "teste",
+                                    type: "image/jpeg",
+                                    uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
+                                }
+                            }
+                        },
+                        {
+                            header: {
+                                type: "application/vnd.lime.media-link+json",
+                                value: {
+                                    title: "teste",
+                                    text: "teste",
+                                    type: "image/jpeg",
+                                    uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
+                                }
+                            }
+                        },
+                    ]
+                }
+            
+        }
       return newObject;
     });
     return filterTweets;
