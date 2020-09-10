afa8b66b1d670e827e951d46434215c0163f8be4
Fernanda Nunes <fernanda.nunes@ibm.com>
2020-09-10 02:16:28 -0300

    test in json

--- connectors/apiGithub.js
+++ connectors/apiGithub.js
@@ -14,93 +14,21 @@ function returnDataGithub() {
                 "header": {
                     "type": "application/vnd.lime.media-link+json",
                     "value": {
-                        "title": "Title",
-                        "text": "This is a first item",
+                        "title": result.name + result.full_name,
+                        "text": result.description,
                         "type": "image/jpeg",
-                        "uri": "http://www.isharearena.com/wp-content/uploads/2012/12/wallpaper-281049.jpg"
+                        "uri": result.avatar_url
                     }
                 },
-                "options": [
-                    {
-                        "label": {
-                            "type": "application/vnd.lime.web-link+json",
-                            "value": {
-                                "title": "Link",
-                                "uri": "http://www.adoteumgatinho.org.br"
-                            }
-                        }
-                    },
-                    {
-                        "label": {
-                            "type": "text/plain",
-                            "value": "{{random.guid}}"
-                        },
-                        "value": {
-                            "type": "application/json",
-                            "value": {
-                                "key1": "value1",
-                                "key2": "2"
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                "header": {
-                    "type": "application/vnd.lime.media-link+json",
-                    "value": {
-                        "title": "Title 2",
-                        "text": "This is another item",
-                        "type": "image/jpeg",
-                        "uri": "http://www.freedigitalphotos.net/images/img/homepage/87357.jpg"
-                    }
-                },
-                "options": [
-                    {
-                        "label": {
-                            "type": "application/vnd.lime.web-link+json",
-                            "value": {
-                                "title": "Second link",
-                                "text": "Weblink",
-                                "uri": "https://pt.dreamstime.com/foto-de-stock-brinquedo-pl%C3%A1stico-amarelo-do-pato-image44982058"
-                            }
-                        }
-                    },
-                    {
-                        "label": {
-                            "type": "text/plain",
-                            "value": "Second text"
-                        },
-                        "value": {
-                            "type": "application/json",
-                            "value": {
-                                "key3": "value3",
-                                "key4": "4"
-                            }
-                        }
-                    },
-                    {
-                        "label": {
-                            "type": "text/plain",
-                            "value": "More one text"
-                        },
-                        "value": {
-                            "type": "application/json",
-                            "value": {
-                                "key5": "value5",
-                                "key6": "6"
-                            }
-                        }
-                    }
-                ]
             }
+            
         ]
     }
       
       
       return newObject;
     });
-    return filterTweets[0];
+    return filterTweets;
   });
 }
 
