518f00453b907d73647a5eb3093af4d4e208b099
Fernanda Nunes <fernanda.nunes@ibm.com>
2020-09-10 00:33:24 -0300

    test in json

--- connectors/apiGithub.js
+++ connectors/apiGithub.js
@@ -6,74 +6,42 @@ function returnDataGithub() {
   url = "https://api.github.com/orgs/takenet/repos?q=language:C#&order=cresc";
   return axios.get(url).then((response) => {
     let filterTweets = response.data.map((result) => {
-      let newObject = {
-        type: "application/vnd.lime.collection+json",
-                to: 'teste', 
-                id: Lime.Guid(),
-                content: {
-                    itemType: "application/vnd.lime.document-select+json",
-                    items: [
-                        {
-                            header: {
-                                type: "application/vnd.lime.media-link+json",
-                                value: {
-                                    title: "teste",
-                                    text: "teste",
-                                    type: "image/jpeg",
-                                    uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
-                                }
-                            
-                            }
-                        },
-                        {
-                            header: {
-                                type: "application/vnd.lime.media-link+json",
-                                value: {
-                                    title: "teste",
-                                    text: "teste",
-                                    type: "image/jpeg",
-                                    uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
-                                }
-                            }
-                        },
-                        {
-
-                            header: {
-                                type: "application/vnd.lime.media-link+json",
-                                value: {
-                                    title: "teste",
-                                    text: "tal",
-                                    type: "image/jpeg",
-                                    uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
-                                }
-                            }
-                        },
-                        {
-                            header: {
-                                type: "application/vnd.lime.media-link+json",
-                                value: {
-                                    title: "teste",
-                                    text: "teste",
-                                    type: "image/jpeg",
-                                    uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
-                                }
-                            }
-                        },
-                        {
-                            header: {
-                                type: "application/vnd.lime.media-link+json",
-                                value: {
-                                    title: "teste",
-                                    text: "teste",
-                                    type: "image/jpeg",
-                                    uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
-                                }
-                            }
-                        },
-                    ]
-                }
-            
-        }
+      let newObject = 
+        {
+          "to": "553199990000@0mn.io",
+          "type": "application/vnd.lime.collection+json",
+          "content": {
+              "itemType": "application/vnd.lime.container+json",
+              "items": [
+                  {
+                      "type": "application/vnd.lime.media-link+json",
+                      "value": {
+                          "text": "Welcome to our store!",
+                          "type": "image/jpeg",
+                          "uri": "http://www.petshoplovers.com/wp-content/uploads/2014/03/CUIDADOS-B%C3%81SICOS-PARA-CRIAR-COELHOS.jpg"
+                      }
+                  },
+                  {
+                      "type": "application/vnd.lime.select+json",
+                      "value": {
+                          "text": "Choose what you need",
+                          "options": [
+                              {
+                                  "order": 1,
+                                  "text": "See our stock"
+                              },
+                              {
+                                  "order": 2,
+                                  "text": "Follow an order"
+                              }
+                          ]
+                      }
+                  }
+              ]
+          }
+      }
+      
+      
       return newObject;
     });
     return filterTweets;
