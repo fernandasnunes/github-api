fe2153bd937190a6a9eac588f67219d7c29a6b9f
Fernanda Nunes <fernanda.nunes@ibm.com>
2020-09-10 21:55:51 -0300

    added return json

--- connectors/apiGithub.js
+++ connectors/apiGithub.js
@@ -5,76 +5,111 @@ const Lime = require('lime-js');
 function returnDataGithub() {
   url = "https://api.github.com/orgs/takenet/repos?q=language:C#&order=cresc";
   return axios.get(url).then((response) => {
-      var newObject = { 
-        type: "application/vnd.lime.collection+json",
-        content: {
-            itemType: "application/vnd.lime.document-select+json",
-            items: [
-                {
-                    header: {
-                        type: "application/vnd.lime.media-link+json",
-                        value: {
-                            title: "Title",
-                            text: "descrição",
-                            type: "image/jpeg",
-                            uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
-                        }
-                    
+      var newObject = {
+        "itemType": "application/vnd.lime.document-select+json",
+        "items": [
+            {
+                "header": {
+                    "type": "application/vnd.lime.media-link+json",
+                    "value": {
+                        "title": "Title",
+                        "text": "This is a first item",
+                        "type": "image/jpeg",
+                        "uri": "http://www.isharearena.com/wp-content/uploads/2012/12/wallpaper-281049.jpg"
                     }
                 },
-                {
-                    header: {
-                        type: "application/vnd.lime.media-link+json",
-                        value: {
-                            title: "teste",
-                            text: "descricao 2",
-                            type: "image/jpeg",
-                            uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
+                "options": [
+                    {
+                        "label": {
+                            "type": "application/vnd.lime.web-link+json",
+                            "value": {
+                                "title": "Link",
+                                "uri": "http://www.adoteumgatinho.org.br"
+                            }
+                        }
+                    },
+                    {
+                        "label": {
+                            "type": "text/plain",
+                            "value": "{{random.guid}}"
+                        },
+                        "value": {
+                            "type": "application/json",
+                            "value": {
+                                "key1": "value1",
+                                "key2": "2"
+                            }
                         }
                     }
-                },
-                {
-
-                    header: {
-                        type: "application/vnd.lime.media-link+json",
-                        value: {
-                          title: "teste",
-                          text: "descricao 2",
-                            type: "image/jpeg",
-                            uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
-                        }
+                ]
+            },
+            {
+                "header": {
+                    "type": "application/vnd.lime.media-link+json",
+                    "value": {
+                        "title": "Title 2",
+                        "text": "This is another item",
+                        "type": "image/jpeg",
+                        "uri": "http://www.freedigitalphotos.net/images/img/homepage/87357.jpg"
                     }
                 },
-                {
-                    header: {
-                        type: "application/vnd.lime.media-link+json",
-                        value: {
-                          title: "teste",
-                          text: "descricao 2",
-                            type: "image/jpeg",
-                            uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
+                "options": [
+                    {
+                        "label": {
+                            "type": "application/vnd.lime.web-link+json",
+                            "value": {
+                                "title": "Second link",
+                                "text": "Weblink",
+                                "uri": "https://pt.dreamstime.com/foto-de-stock-brinquedo-pl%C3%A1stico-amarelo-do-pato-image44982058"
+                            }
+                        }
+                    },
+                    {
+                        "label": {
+                            "type": "text/plain",
+                            "value": "Second text"
+                        },
+                        "value": {
+                            "type": "application/json",
+                            "value": {
+                                "key3": "value3",
+                                "key4": "4"
+                            }
+                        }
+                    },
+                    {
+                        "label": {
+                            "type": "text/plain",
+                            "value": "More one text"
+                        },
+                        "value": {
+                            "type": "application/json",
+                            "value": {
+                                "key5": "value5",
+                                "key6": "6"
+                            }
                         }
                     }
-                },
-                {
-                    header: {
-                        type: "application/vnd.lime.media-link+json",
-                        value: {
-                          title: "teste",
-                          text: "descricao 2",
-                            type: "image/jpeg",
-                            uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
-                        }
-                    }
-                },
-            ]
-        }
+                ]
+            }
+        ]
     };
       
   return newObject
   
     
 
+    // let formatArray = filterTweets.replace(/[[\]]/g,'');
+    // let arraySSP = (JSON.parse(formatArray))
+    // return arraySS P
+    // filterTweets)
+
+
+  //   function formatData(array) {
+  //     let formatArray = array.replace(/[[\]]/g,'')
+  //     let arraySSP = (JSON.parse(formatArray))
+  //     return arraySSP
+  // }
 
   
   });
