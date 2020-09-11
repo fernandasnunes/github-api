370515c0e5ae7bc999f080e32f37d4a678d4d9ac
Fernanda Nunes <fernanda.nunes@ibm.com>
2020-09-10 21:47:26 -0300

    added return json

--- connectors/apiGithub.js
+++ connectors/apiGithub.js
@@ -5,42 +5,77 @@ const Lime = require('lime-js');
 function returnDataGithub() {
   url = "https://api.github.com/orgs/takenet/repos?q=language:C#&order=cresc";
   return axios.get(url).then((response) => {
-    let filterTweets = response.data.map((result) => {
-      let newObject = 
-      {
-        "itemType": "application/vnd.lime.document-select+json",
-        "items": [
-            {
-                "header": {
-                    "type": "application/vnd.lime.media-link+json",
-                    "value": {
-                        "title": result.name + result.full_name,
-                        "text": result.description,
-                        "type": "image/jpeg",
-                        "uri": result.owner.avatar_url
+      var newObject = { 
+        type: "application/vnd.lime.collection+json",
+        content: {
+            itemType: "application/vnd.lime.document-select+json",
+            items: [
+                {
+                    header: {
+                        type: "application/vnd.lime.media-link+json",
+                        value: {
+                            title: "Title",
+                            text: "descrição",
+                            type: "image/jpeg",
+                            uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
+                        }
+                    
                     }
                 },
-            }
-            
-        ]
-    }
+                {
+                    header: {
+                        type: "application/vnd.lime.media-link+json",
+                        value: {
+                            title: "teste",
+                            text: "descricao 2",
+                            type: "image/jpeg",
+                            uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
+                        }
+                    }
+                },
+                {
+
+                    header: {
+                        type: "application/vnd.lime.media-link+json",
+                        value: {
+                          title: "teste",
+                          text: "descricao 2",
+                            type: "image/jpeg",
+                            uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
+                        }
+                    }
+                },
+                {
+                    header: {
+                        type: "application/vnd.lime.media-link+json",
+                        value: {
+                          title: "teste",
+                          text: "descricao 2",
+                            type: "image/jpeg",
+                            uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
+                        }
+                    }
+                },
+                {
+                    header: {
+                        type: "application/vnd.lime.media-link+json",
+                        value: {
+                          title: "teste",
+                          text: "descricao 2",
+                            type: "image/jpeg",
+                            uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
+                        }
+                    }
+                },
+            ]
+        }
+    };
       
   return newObject
   
-    });
-
-    // let formatArray = filterTweets.replace(/[[\]]/g,'');
-    // let arraySSP = (JSON.parse(formatArray))
-    // return arraySS P
-    let json = JSON.stringify(filterTweets).slice(1, -1)
+    
 
 
-  //   function formatData(array) {
-  //     let formatArray = array.replace(/[[\]]/g,'')
-  //     let arraySSP = (JSON.parse(formatArray))
-  //     return arraySSP
-  // }
-    return json
   
   });
 }
